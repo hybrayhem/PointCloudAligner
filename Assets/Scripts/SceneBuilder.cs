accf912c6624dd002e9f60ed6038b4cc1ce05ab9
Halil İLHAN <47319650+hybrayhem@users.noreply.github.com>
2023-12-03 23:31:59 +0300

    parse and visualize points

--- Assets/Scripts/SceneBuilder.cs
+++ Assets/Scripts/SceneBuilder.cs
@@ -16,8 +16,11 @@ public class SceneBuilder : MonoBehaviour
         // Print Hello
         Debug.Log("Hello");
 
-        readPointCloud("octahedron14");
-        readPointCloud("octahedron6");
+        pointCloud = readPointCloud("octahedron14");
+        pointCloud = readPointCloud("octahedron6");
+
+        printPointCloud(pointCloud);
+        visualizePointCloud(pointCloud);
     }
 
     // Update is called once per frame
@@ -26,19 +29,79 @@ public class SceneBuilder : MonoBehaviour
         
     // }
 
-    void readPointCloud(string filename) {
-        Debug.Log("Reading point cloud " + filename);
+    Vector3[] readPointCloud(string filename) {
+        Vector3[] vectors;
 
         // Get text from file
+        string data;
         string filePath = Application.dataPath + "/PointClouds/" + filename + ".xyz";
+        
         if (File.Exists(filePath)) {
-            string data = File.ReadAllText(filePath);
+            data = File.ReadAllText(filePath);
             Debug.Log(data);
         } else {
             Debug.Log("Can't find file at " + filePath);
+            return null;
         }
 
-        // Parse into vector3 array
+        // Parse text into vector3 array
+        var lines = data.Split('\n');
+        vectors = new Vector3[lines.Length];
 
+        for (int i = 0; i < lines.Length; i++) {
+            var line = lines[i];
+            var coordinates = line.Split(' ');
+
+            var x = float.Parse(coordinates[0]);
+            var y = float.Parse(coordinates[1]);
+            var z = float.Parse(coordinates[2]);
+
+            vectors[i] = new Vector3(x, y, z);
+        }
+
+        return vectors;
+    }
+
+    void printPointCloud(Vector3[] pointCloud) {
+        foreach (var point in pointCloud) {
+            Debug.Log(point);
+        }
+    }
+
+    void visualizePointCloud(Vector3[] pointCloud) {
+        // To fit point cloud in camera view
+        var visualizationOrigin = new Vector3(0, 0, 7);
+        var downscaleRatio = 0.1f;
+        var pointSize = 0.1f;
+
+        Vector3[] newPointCloud;
+        newPointCloud = scalePointCloud(pointCloud, downscaleRatio);
+        newPointCloud = translatePointCloud(newPointCloud, visualizationOrigin);
+
+        foreach (var point in newPointCloud) {
+            // Instantiate a sphere
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.position = point;
+            sphere.transform.localScale = new Vector3(pointSize, pointSize, pointSize);
+        }
+    }
+
+    Vector3[] scalePointCloud(Vector3[] pointCloud, float scale) {
+        Vector3[] scaledPointCloud = new Vector3[pointCloud.Length];
+
+        for(int i = 0; i < pointCloud.Length; i++) {
+            scaledPointCloud[i] = pointCloud[i] * scale;
+        }
+        return scaledPointCloud;
+    }
+
+    Vector3[] translatePointCloud(Vector3[] pointCloud, Vector3 translation) {
+        Vector3[] translatedPointCloud = new Vector3[pointCloud.Length];
+
+        for(int i = 0; i < pointCloud.Length; i++) {
+            translatedPointCloud[i] = pointCloud[i] + translation;
+        }
+
+        return translatedPointCloud;
     }
 }
